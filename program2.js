42c506991d6a1a25e291b54e9a2aee35288185d9
sdn-krishna <singasanidurganitishkrishna@gmail.com>
2024-11-11 13:22:39 +0530

    Initial commit for coding assessment

--- program2.js
+++ program2.js
@@ -1,6 +1,31 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
+    const m = s.length;
+    const n = p.length;
+
+
+    const dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
+
+    dp[0][0] = true;
+
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (p[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            } else if (p[j - 1] === '?' || p[j - 1] === s[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+        }
+    }
+
+    return dp[m][n];
 
   };
   
